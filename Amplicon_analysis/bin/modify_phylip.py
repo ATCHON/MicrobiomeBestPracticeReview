bedb90cca95a701143742b2066119906808225e9
Sourcery AI <>
2022-04-12 08:56:12 +0000

    'Refactored by Sourcery'

--- Amplicon_analysis/bin/modify_phylip.py
+++ Amplicon_analysis/bin/modify_phylip.py
@@ -1,5 +1,5 @@
-from Bio import SeqIO        
-import re            
+from Bio import SeqIO
+import re
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -15,12 +15,12 @@ fasta = SeqIO.parse(handle=fasta_file, format='fasta')
 for seq in fasta:
     # print(seq.description)
     otu = re.search('(Otu[0-9]+)\|[0-9]+', seq.description)
-    # print(otu.group(1))  
-    seq.id = otu.group(1)
+    # print(otu.group(1))
+    seq.id = otu[1]
     new_names.append(seq)
 
 newfasta_name = str(fasta_file)
 nu = newfasta_name.find('fasta')
-_new_fastafile = newfasta_name[0:nu] + 'otu_modified.fasta'
+_new_fastafile = newfasta_name[:nu] + 'otu_modified.fasta'
 #print (self._new_fastafile)
 SeqIO.write(sequences=new_names, format='fasta', handle=_new_fastafile)  
